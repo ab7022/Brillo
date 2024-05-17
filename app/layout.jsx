4df7b1f8fe8e7dd2398e8215e9b102d2031bcb38
ab7022 <bayees1@gmail.com>
2024-05-18 03:44:11 +0530

    trying to deploy

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,6 @@ import { Toaster } from 'react-hot-toast';
 import { Arimo, Rubik } from 'next/font/google';
 import { Providers } from './Providers';
 import ResumeContextProvider from '@/components/context/ResumeData';
-import ErrorBoundary from '@/components/ErrorBoundary'
 import './globals.css'
 const arimo = Arimo({
   subsets: ['latin'],
