f9972483de46a1d6a08bb3c1a2c4f5a938100f43
ab7022 <bayees1@gmail.com>
2024-06-27 03:30:19 +0530

    TryingForDeploy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,26 @@
 "use client";
-import Dashboard from "@/components/dashboard";
-import Header from "@/components/HomePage/Header";
+import DashboardComponent from "@/components/dashboard";
+import HeaderComponent from "@/components/HomePage/Header";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
-export default function dashboard() {
+export default function Dashboard() {
   const { data: session, status } = useSession();
+  
   if (!session && status === "loading") {
     return <div>Loading...</div>;
   }
 
   if (!session) {
     redirect("/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F");
-    return null
+    return null;
   }
+
   return (
     <>
-      <Header session={session} />
+      <HeaderComponent session={session} />
       <div className="md:py-24 py-20">
-        <Dashboard session={session} />
+        <DashboardComponent session={session} />
       </div>
     </>
   );
