65d2b09b65b9a917f5a6e1708dd414175464771e
ab7022 <bayees1@gmail.com>
2024-07-09 02:10:32 +0530

    modified template details page

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -36,7 +36,7 @@ const Template1 = async () => {
           {/* Themes Grid */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {TemplateData.map((template, index) => (
-              <Template key={template.id} {...template} index={index} />
+              <Template link={undefined} key={template.id} {...template} index={index} />
             ))}
           </div>
 
